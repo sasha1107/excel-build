253c6125d861ca4f3c194be0d00d43839dd44fd5
정수현 <sasha1107@naver.com>
2023-12-26 17:01:20 +0900

    Style: FloatButton z-index 지정

--- docs/src/app/_components/FloatButton.tsx
+++ docs/src/app/_components/FloatButton.tsx
@@ -6,7 +6,7 @@ import TopIcon from '@/assets/top.svg';
 const FloatButton = () => {
   return (
     <button
-      className='fixed bg-transparent right-8 bottom-8 bg-primary-400 p-3 rounded-full text-lg text-primary-800'
+      className='fixed bg-transparent right-8 bottom-8 bg-primary-400 p-3 rounded-full text-lg text-primary-800 z-50'
       onClick={() => {
         window.scrollTo({
           top: 0,
