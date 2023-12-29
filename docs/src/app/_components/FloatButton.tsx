30908a5e0d2c79121304d654a762ea0733244699
정수현 <sasha1107@naver.com>
2023-12-29 13:07:30 +0900

    style: FloatButton 스타일 수정

--- docs/src/app/_components/FloatButton.tsx
+++ docs/src/app/_components/FloatButton.tsx
@@ -6,7 +6,7 @@ import TopIcon from '@/assets/top.svg';
 const FloatButton = () => {
   return (
     <button
-      className='fixed bg-transparent right-8 bottom-8 bg-primary-400 p-3 rounded-full text-lg text-primary-800 z-50'
+      className='fixed right-8 bottom-8 bg-primary-300 p-3 rounded-full text-lg text-primary-800 border border-priamry-100 font-lg w-11 h-11'
       onClick={() => {
         window.scrollTo({
           top: 0,
