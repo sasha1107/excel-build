b6b3ad3173eeb73c1bc1644b873fcda7d984d02b
정수현 <sasha1107@naver.com>
2024-01-02 14:38:45 +0900

    Chore: v1.0.23 gulp 적용

--- packages/gulpfile.js
+++ packages/gulpfile.js
@@ -1,8 +1,23 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
+const terser = require('gulp-terser');
 
 gulp.task('minify', function () {
-  return gulp.src('dist/lib/*').pipe(uglify()).pipe(gulp.dest('dist/lib'));
+  return gulp
+    .src(['dist/*', '!dist/*.d.ts'])
+    .pipe(uglify())
+    .pipe(
+      terser({
+        compress: {
+          drop_console: true,
+        },
+        output: {
+          beautify: false,
+          comments: false,
+        },
+      })
+    )
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', gulp.series('minify'));
+gulp.task('default', gulp.series(['minify']));
